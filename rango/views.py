6bb01eecd5284e06c3384c33bb8e9c92d5dd94b6
Elena Valkova <2087668v@student.gla.ac.uk>
2018-02-04 20:23:09 +0000

    Included most viewed pages in the index (Excercises after Chapter 6)

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,10 @@ from rango.models import Category
 from rango.models import Page
 
 def index(request):
+    context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages =  Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages':pages}
     return render(request, 'rango/index.html', context_dict)
 def about(request):
     return render(request, 'rango/about.html')
