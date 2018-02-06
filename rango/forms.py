cc15316b00f93e820c7a3889ae022e678fc93862
Elena Valkova <2087668v@student.gla.ac.uk>
2018-02-06 16:30:17 +0000

    Used templates (Finished chapter 8)

--- rango/forms.py
+++ rango/forms.py
@@ -24,17 +24,17 @@ class PageForm(forms.ModelForm):
         if url and not url.startswith('http://'):
             url = 'http://' + url
             cleaned_data['url'] = url
-            
+
             return cleaned_data
 
-class Meta:
-    model = Page
+    class Meta:
+        model = Page
     # What fields do we want to include in our form?
     # This way we don't need every field in the model present.
     # Some fields may allow NULL values, so we may not want to include them.
     # Here, we are hiding the foreign key.
      # we can either exclude the category field from the form,
-    exclude = ('category',)
+        exclude = ('category',)
     # or specify the fields to include (i.e. not include the category field)
     #fields = ('title', 'url', 'views')
 
