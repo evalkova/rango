4c6fc6c77d879055703c00b986eebf93aa9d2f48
Elena Valkova <2087668v@student.gla.ac.uk>
2018-02-08 14:05:21 +0000

    Added user authentication (Chapter 9)

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 
 admin.site.register(Category)
+admin.site.register(UserProfile)
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 #Media file hosting
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/rango/login/'
