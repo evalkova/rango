f4da24e9d6b813a45309311aa38b33f6e7361763
Elena Valkova <2087668v@student.gla.ac.uk>
2018-02-08 16:11:06 +0000

    Attempted Chapter 9 Excercises

--- rango/views.py
+++ rango/views.py
@@ -32,6 +32,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     # A HTTP POST?
@@ -48,6 +49,7 @@ def add_category(request):
     # Will handle the bad form, new form, or no form supplied cases.
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -173,8 +175,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
-
+    return render(request, 'rango/restricted.html', {})
     # Use the login_required() decorator to ensure only those logged in can
 # access the view.
 @login_required
